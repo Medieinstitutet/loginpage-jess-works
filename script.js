d373b9f0ff273c589e808b580cf18f3b124bf72d
Jessika Ljungberg <jessika.lljungberg@gmail.com>
2021-12-05 20:04:36 +0100

    if/else
    behöver testa vad som händer om jajg tar bort rad 14 och 15

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-/* 
- vy ett --> addEventListener("click", ?) ger att rätt username och password
- leder till vy två 
- vy två --> Rutor bort, logga ut knapp (Hej username)
- addEventListener("click", ?) ger att user loggas ut och tas tillbaka till vy ett
- vy tre --> vid fel username eller password, behöver leda till vy två vid rätt credentials
- if/if else
- lägg till localStorage
- */
+ let username = document.getElementById("username");
+ let password = document.getElementById("password");
+ let login = document.getElementById("login");
+ let btnLogin = document.getElementById("btnLogin");
+ let btnLogout = document.getElementById("btnLogout");
 
- //lyft in variabler i js
+ let notLoggedIn = document.getElementById("notLoggedIn");
+ let wrongLogin = document.getElementById("wrongLogin");
+ let loggedIn = document.getElementById("loggedIn");
 
- const username = document.addElementById("username");
- const password = document.addElementById("password");
- const login = document.addElementById("login");
- const btnLogin = document.getElementById("btnLogin");
- const btnLogout = document.getElementById("btnLogout");
+ //default toggle som döljer vy 2 och 3 i startläge. HUR?
 
- const notLoggedIn = document.getElementById("notLoggedIn");
- const wrongLogin = document.getElementById("wrongLogin");
- const loggedIn = document.getElementById("loggedIn");
+ btnLogin.addEventListener("click", function () {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-//login i vy ett som leder till vy två
+    if (username === "janne" && password === "test") {
+        document.getElementById("username").style.display = "none";
+        document.getElementById("password").style.display = "none";
+        document.getElementById("btnLogin").style.display = "none";
+        document.getElementById("notLoggedIn").style.display = "none";
+        document.getElementById("wrongLogin").style.display = "none";
+        document.getElementById("loggedIn").style.display = "block";
 
+        //localStorage save? Stringify
+        //localStorage.setItem("username", "janne");
+    }
+    else {
+        document.getElementById("btnLogout").style.display = "none";
+        document.getElementById("notLoggedIn").style.display = "none";
+        document.getElementById("wrongLogin").style.display = "block";
+        document.getElementById("loggedIn").style.display = "none";
+    }
+ });
 
-//logout i vy två leder tillbaka till vy ett
-
-
-//felmeddelande i vy tre --> fel igen leder till samma felmeddelande. Rätt leder till vy två
+ btnLogout.addEventListener("click", function () {
+     document.getElementById("username").style.display = "block";
+     document.getElementById("password").style.display = "block";
+     document.getElementById("btnLogin").style.display = "block";
+     document.getElementById("btnLogout").style.display = "none";
+     document.getElementById("notLoggedIn").style.display = "block";
+     document.getElementById("wrongLogin").style.display = "none";
+     document.getElementById("loggedIn").style.dispaly = "none";
+     //varför syns "loggedIn"?
+    //localStorage erase?
+ })
